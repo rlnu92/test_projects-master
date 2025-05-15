d21ce3898ac317b9d133968249ac3e4c4ab0679c
rlnu92 <ramandeepkaur.lnu17@rdpolytech.ca>
2025-05-14 19:12:49 -0600

    Update

---
+++ test_projects-master/src/functions/api.test.js
@@ -0,0 +1,17 @@
+const fetchData = require(',,/functions/api.js');
+
+
+//Test Suite
+describe('fetchData', () =>{
+
+    //Test cases
+    it('fetches data fron an API endpoint and returns it as JSON', () =>{
+        //mock the function response
+          const mockResponses = {userId:1, id:1, title: 'Test Title', completed: false};
+    });
+
+
+
+
+});
+    
